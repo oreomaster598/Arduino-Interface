066ac318b9dac5f0a98143bcba34bdc4270ca7ea
oreomaster598 <61253820+oreomaster598@users.noreply.github.com>
2021-05-17 17:47:33 -0500

    Add files via upload

---
+++ Arduino/Arduino.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+
+namespace Arduino
+{
+    public class Arduino
+    {
+        private SerialPort serialPort;
+        public string log;
+        public bool PrintPortStream = false;
+        public string name;
+
+        public Arduino(string portName)
+        {
+            serialPort = new SerialPort(portName);
+            serialPort.BaudRate = 9600;
+            serialPort.ReadTimeout = 2000;
+            serialPort.WriteTimeout = 2000;
+            serialPort.DataReceived += DataReceived;
+            serialPort.Open();
+            name = portName;
+        }
+        private void DataReceived(object s, SerialDataReceivedEventArgs e)
+        {
+            byte[] data = new byte[1024];
+            int bytesRead = serialPort.Read(data, 0, data.Length);
+            if(PrintPortStream)
+                Console.Write(Encoding.ASCII.GetString(data, 0, bytesRead));
+            log+=Encoding.ASCII.GetString(data, 0, bytesRead);
+        }
+        public void Write(string text)
+        {
+            serialPort.WriteLine(text);
+        }
+    }
+}
---
+++ Arduino/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+using System.Threading;
+
+namespace Arduino
+{
+    class Program
+    {
+        public static string help = "Syntax: Arduino PortName [PrintPortStream (true/false)]";
+
+        static void Main(string[] args)
+        {
+            if(args.Length >= 1)
+            {
+                Arduino arduino = new Arduino(args[0]);
+                if(args.Length >= 2)
+                 if(args[1] == "true")
+                    arduino.PrintPortStream = true;
+
+                while(true)
+                {
+                    Console.Write("Write to arduino: ");
+                    string str = Console.ReadLine();
+                    Console.Clear();
+                    arduino.Write(str);
+                    Thread.Sleep(75);
+                    Console.WriteLine($"Arduino ({arduino.name}) Connected\n"+arduino.log);
+                }
+            }
+            else
+            {
+                Console.WriteLine(help);
+            }
+        }
+    }
+}
