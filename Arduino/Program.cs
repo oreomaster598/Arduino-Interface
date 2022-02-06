f2d7052e2dc1b9ecb95d309a5e6ad2c3f6c1bc7b
oreomaster598 <61253820+oreomaster598@users.noreply.github.com>
2022-02-05 20:53:51 -0600

    Update Program.cs

--- Arduino/Program.cs
+++ Arduino/Program.cs
@@ -10,25 +10,25 @@ namespace Arduino
 {
     class Program
     {
-        public static string help = "Syntax: Arduino PortName [PrintPortStream (true/false)]";
+        public static string help = "Syntax: Arduino PortName";
 
         static void Main(string[] args)
         {
             if(args.Length >= 1)
             {
                 Arduino arduino = new Arduino(args[0]);
-                if(args.Length >= 2)
-                 if(args[1] == "true")
-                    arduino.PrintPortStream = true;
 
                 while(true)
                 {
-                    Console.Write("Write to arduino: ");
-                    string str = Console.ReadLine();
-                    Console.Clear();
-                    arduino.Write(str);
-                    Thread.Sleep(75);
-                    Console.WriteLine($"Arduino ({arduino.name}) Connected\n"+arduino.log);
+                    Console.Write(arduino.name+"> ");
+                    string inp = Console.ReadLine();
+                    if(inp == "exit")
+                        Environment.Exit(0);
+                    else
+                    {
+                        arduino.WriteToArduino(inp);
+                        Console.WriteLine(arduino.ReadFromArduino());
+                    }
                 }
             }
             else
